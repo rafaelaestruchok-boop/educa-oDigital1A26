95ae1657124de0629c976781795ca0e816d34290
rafaelaestruchok-boop <rafaela.estruchok@escola.pr.gov.br>
2026-08-06 10:31:38 -0300

    alterei

--- script.js
+++ script.js
@@ -15,3 +15,17 @@
             }
         }
     });
+
+      const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
+
+ btnTemaEscuro.addEventListener("click", mudaTema);
+
+ function mudaTema() {
+     const corpoPagina = document.body;
+
+    if (corpoPagina.classList.contains("tema-escuro")) {
+        corpoPagina.classList.remove("tema-escuro");
+    } else {
+        corpoPagina.classList.add("tema-escuro");
+    }
+ }
